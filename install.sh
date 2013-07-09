3f25536da22bacaf18322ffe44312f1abbc283cb
Wentao Han <wentao.han@gmail.com>
2013-07-09 09:41:29 +0800

    Back up config files if exist

--- install.sh
+++ install.sh
@@ -9,6 +9,10 @@ CONF_FILE_LIST=".profile .bashrc .gitconfig .tmux.conf .vimrc .vim"
 
 for conf_file in $CONF_FILE_LIST; do
     conf_file2="${conf_file/#./_}"
+    if [ -e $conf_file ]; then
+        echo "Backing up $HOME/$conf_file to $HOME/$conf_file.old"
+        mv -f $HOME/$conf_file $HOME/$conf_file.old
+    fi
     echo "Creating symlink for $PWD/$conf_file2 to $HOME/$conf_file"
     ln -sf $PWD/$conf_file2 $HOME/$conf_file
 done
